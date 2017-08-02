e6140869687a25e308d0fe35ccbbbc73139b44b5
craigmiller160 <craigmiller160@gmail.com>
2017-08-02 17:32:12 -0400

    Added documentation to Synchronize

--- src/io/craigmiller160/kotlin/commons/concurrent/Synchronize.kt
+++ src/io/craigmiller160/kotlin/commons/concurrent/Synchronize.kt
@@ -3,6 +3,11 @@ package io.craigmiller160.kotlin.commons.concurrent
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+/**
+ * A property delegating class that ensures all
+ * access to the property is synchronized on
+ * the object that owns the property.
+ */
 class Synchronize<T>(defaultValue: T) : ReadWriteProperty<Any, T> {
 
     private var backingField = defaultValue
