e5153308c56ea04e634d49abef9a84786d525686
craigmiller160 <craigmiller160@gmail.com>
2017-08-03 15:52:56 -0400

    Added another unit test for StatementUtils

--- src/test/kotlin/io/craigmiller160/kotlin/commons/jdbc/StatementUtilsTest.kt
+++ src/test/kotlin/io/craigmiller160/kotlin/commons/jdbc/StatementUtilsTest.kt
@@ -7,7 +7,9 @@ import java.io.File
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
+import java.sql.Statement
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class StatementUtilsTest {
@@ -37,7 +39,9 @@ class StatementUtilsTest {
     @Test
     fun testUseQuickQuery(){
         var recordCount = 0
-        conn.createStatement().use { stmt ->
+        var stmt: Statement? = null
+        try{
+            stmt = conn.createStatement()
             var result: ResultSet? = null
             stmt.useQuickQuery("SELECT * FROM people ORDER BY person_id ASC"){ rs ->
                 result = rs
@@ -50,10 +54,38 @@ class StatementUtilsTest {
                 assertEquals("John", rs.getString("first_name"), "Wrong value for second first_name")
             }
 
+            assertEquals(2, recordCount, "Wrong number of records found")
             assertTrue("ResultSet did not get closed") { result?.isClosed ?: false }
+            assertFalse("Statement should not be closed") { stmt?.isClosed ?: false }
         }
+        finally{
+            stmt?.close()
+        }
+    }
 
-        assertEquals(2, recordCount, "Wrong number of records found")
+    @Test
+    fun testUseQuickQueryItr(){
+        var recordCount = 0
+        var stmt: Statement? = null
+        try{
+            stmt = conn.createStatement()
+            stmt.useQuickQueryItr("SELECT * FROM people ORDER BY person_id ASC") { rs ->
+                rs.forEachIndexed { i, rs ->
+                    recordCount++
+                    when(i){
+                        0 -> assertEquals("Bob", rs.getString("first_name"), "Wrong value for first first_name")
+                        1 -> assertEquals("John", rs.getString("first_name"), "Wrong value for second first_name")
+                        else -> throw Exception("Invalid index: " + i)
+                    }
+                }
+            }
+
+            assertEquals(2, recordCount, "Wrong number of records found")
+            assertFalse("Statement should not be closed") { stmt?.isClosed ?: false }
+        }
+        finally{
+            stmt?.close()
+        }
     }
 
 }
