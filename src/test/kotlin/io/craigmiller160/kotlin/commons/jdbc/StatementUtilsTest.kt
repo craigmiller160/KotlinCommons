c0c3eff3b56772decac316cd641b2091b669da49
craigmiller160 <craigmiller160@gmail.com>
2017-08-03 15:24:02 -0400

    Bumped the pom.xml up a minor version

--- src/test/kotlin/io/craigmiller160/kotlin/commons/jdbc/StatementUtilsTest.kt
+++ src/test/kotlin/io/craigmiller160/kotlin/commons/jdbc/StatementUtilsTest.kt
@@ -8,7 +8,6 @@ import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class StatementUtilsTest {
