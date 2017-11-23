417bcb2b93499f56abc3f0954ee2a65503bebb57
craigmiller160 <craigmiller160@gmail.com>
2017-11-23 08:10:46 -0500

    Added documentation to CollectionUtils

--- src/main/kotlin/io/craigmiller160/kotlin/commons/collection/CollectionUtils.kt
+++ src/main/kotlin/io/craigmiller160/kotlin/commons/collection/CollectionUtils.kt
@@ -1,5 +1,13 @@
 package io.craigmiller160.kotlin.commons.collection
 
+/**
+ * Transform the contents of a Sequence by applying
+ * the provided function to each element.
+ *
+ * @param transform the function to transform each
+ *                  element of the Sequence.
+ * @return a new Sequence with transformed contents.
+ */
 fun <T> Sequence<T>.transform(transform: (T) -> T): Sequence<T>{
     val list = toList()
     return object : Sequence<T>{
@@ -7,6 +15,14 @@ fun <T> Sequence<T>.transform(transform: (T) -> T): Sequence<T>{
     }
 }
 
+/**
+ * Transform the contents of an Iterable by applying
+ * the provided function to each element.
+ *
+ * @param transform the function to transform each
+ *                  element of the Iterable.
+ * @return a new List with the transformed contents.
+ */
 fun <T> Iterable<T>.transform(transform: (T) -> T): List<T>{
     val itr = this
     return ArrayList<T>().apply { itr.forEach { item -> this += transform(item) } }
