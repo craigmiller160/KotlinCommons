cdaa5e34b9cdd086c6016a92c39c7c81b2d18bf5
craigmiller160 <craigmiller160@gmail.com>
2018-02-21 10:08:58 -0500

    Added documentation to ResourceUtils

--- src/main/kotlin/io/craigmiller160/kotlin/commons/resource/ResourceUtils.kt
+++ src/main/kotlin/io/craigmiller160/kotlin/commons/resource/ResourceUtils.kt
@@ -3,10 +3,26 @@ package io.craigmiller160.kotlin.commons.resource
 import java.io.InputStream
 import java.net.URL
 
+/**
+ * A simple group of shortcut functions to loading
+ * resources from the classpath.
+ */
 object Resource {
 
+    /**
+     * Get a resource on the classpath as a URL.
+     *
+     * @param name the path to the resource.
+     * @return the URL for the resource.
+     */
     fun getResource(name: String): URL = Resource::class.java.classLoader.getResource(name)
 
+    /**
+     * Get a resource on the classpath as an InputStream.
+     *
+     * @param name the path to the resource.
+     * @return the InputStream for the resource.
+     */
     fun getResourceAsStream(name: String): InputStream = Resource::class.java.classLoader.getResourceAsStream(name)
 
 }
