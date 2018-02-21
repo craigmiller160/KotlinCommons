d50565983257999cd490a23dfb0f0e0381e380b7
craigmiller160 <craigmiller160@gmail.com>
2018-02-21 10:10:54 -0500

    Added another method to get a resource as a String

--- src/main/kotlin/io/craigmiller160/kotlin/commons/resource/ResourceUtils.kt
+++ src/main/kotlin/io/craigmiller160/kotlin/commons/resource/ResourceUtils.kt
@@ -1,7 +1,11 @@
 package io.craigmiller160.kotlin.commons.resource
 
+import io.craigmiller160.kotlin.commons.io.readText
+import io.craigmiller160.kotlin.commons.io.useReadText
 import java.io.InputStream
 import java.net.URL
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 
 /**
  * A simple group of shortcut functions to loading
@@ -24,5 +28,16 @@ object Resource {
      * @return the InputStream for the resource.
      */
     fun getResourceAsStream(name: String): InputStream = Resource::class.java.classLoader.getResourceAsStream(name)
+
+    /**
+     * Get a resource on the classpath as a String. This
+     * expects the resource to be purely text content.
+     *
+     * @param name the path to the resource.
+     * @charset the character encoding of the resource,
+     *          defaults to UTF-8.
+     * @return the String of the resource.
+     */
+    fun getResourceAsString(name: String, charset: Charset = StandardCharsets.UTF_8): String = getResourceAsStream(name).useReadText(charset)
 
 }
